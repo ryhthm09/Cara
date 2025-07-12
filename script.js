896490dc5db8b4471538b148ce1d4a15696b09be
ryhthm09 <anandsharma1123@gmail.com>
2025-07-12 13:41:35 +0530

    Made the website responsive

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 // Script for navigation bar
+const bar = document.getElementById("bar");
+const close = document.getElementById("close");
+const navbar = document.getElementById("navbar");
+if(close) {
+    close.addEventListener("click", () => {
+        navbar.classList.remove("active");
+    });
+}
+
+if(bar) {
+    bar.addEventListener("click", () => {
+        navbar.classList.add("active");
+        
+    });
+}
